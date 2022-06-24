c5f72257d4c9b360cf7aa7c3d2093e65004be0ed
Oybek <oybekjonxayrullayevich@gmail.com>
2022-06-24 22:55:38 +0500

    html end

--- src/cloneMain/ListVideos.jsx
+++ src/cloneMain/ListVideos.jsx
@@ -1,12 +1,15 @@
-import React from 'react';
+import React from "react";
+import SideVideos from "./SideVideos";
 
-const ListVideos = () => {
-  return (
-    <div>
-      
-    </div>
-  );
+const ListVideos = ({ videos, onVideoSelect }) => {
+  const videoLists = videos.map((video) => (
+    <SideVideos
+      onVideoSelect={onVideoSelect}
+      key={video.id.videoId}
+      video={video}
+    />
+  ));
+  return <div>{videoLists}</div>;
 };
-
 
 export default ListVideos;
--- src/cloneMain/SideVideos.jsx
+++ src/cloneMain/SideVideos.jsx
@@ -1,13 +1,16 @@
-import React from 'react';
+import React from "react";
 
-const SideVideos = () => {
+const SideVideos = ({ video, onVideoSelect }) => {
   return (
     <div>
-      
+      <div className="videoItems" onClick={() => onVideoSelect(video)}>
+        <img src={video.snippet.thumbnails.medium.url} alt="sss" />
+        <div className="titleVideo">
+          <b>{video.snippet.title}</b>
+        </div>
+      </div>
     </div>
   );
 };
-
-
 
 export default SideVideos;
